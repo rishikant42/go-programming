d79c2947dee33b60b12be6ac819ac53b3081fe37
rishikant42 <rshkntshrm@gmail.com>
2020-01-04 23:45:10 +0530

    card app init

---
+++ go-udemy/cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var abc int = 2
+
+func main() {
+	// var card string = "Ace of spades."
+	card := "Ace of spades."
+	card = "Ace spades."
+	fmt.Println(card)
+	fmt.Println(abc)
+}
