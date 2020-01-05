06e0db7c34f9fd21b0499bcd4480e0216ccea917
rishikant42 <rshkntshrm@gmail.com>
2020-01-05 16:22:31 +0530

    random number generator

--- go-udemy/cards/deck.go
+++ go-udemy/cards/deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // create a new type of 'deck' which is slice of strings
 
@@ -28,4 +35,32 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	currentTime := time.Now()
+	source := rand.NewSource(currentTime.UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- go-udemy/cards/main.go
+++ go-udemy/cards/main.go
@@ -2,8 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingCard := deal(cards, 5)
-	hand.print()
-	remainingCard.print()
+	cards.shuffle()
+	cards.print()
 }
