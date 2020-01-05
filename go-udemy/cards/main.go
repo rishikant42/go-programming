8a16141547a08b31b1d5525a6eccd63d7aafd7da
rishikant42 <rshkntshrm@gmail.com>
2020-01-05 12:18:51 +0530

    newDeck method

--- go-udemy/cards/main.go
+++ go-udemy/cards/main.go
@@ -1,13 +1,6 @@
 package main
 
-import "fmt"
-
-var abc int = 2
-
 func main() {
-	// var card string = "Ace of spades."
-	card := "Ace of spades."
-	card = "Ace spades."
-	fmt.Println(card)
-	fmt.Println(abc)
+	cards := newDeck()
+	cards.print()
 }
