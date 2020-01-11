1c55cb42f499c25022f51ff18ab941a000f89d5b
rishikant42 <rshkntshrm@gmail.com>
2020-01-11 21:37:28 +0530

    struct

---
+++ go-udemy/structs/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstName = newName
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 321001,
+		},
+	}
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
