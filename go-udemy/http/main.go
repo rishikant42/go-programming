1f0eba509667c2f02147f5dcc3d978190d39ab70
rishikant42 <rshkntshrm@gmail.com>
2020-01-19 11:28:06 +0530

    http

--- go-udemy/http/main.go
+++ go-udemy/http/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -12,5 +21,15 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	// method-1
+	// bs := make([]byte, 999999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	// method-2
+	// io.Copy(os.Stdout, resp.Body)
+
+	// method-3: custom writer
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
 }
